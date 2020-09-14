c38c2b52978654440b31407aa5c7157dc0829de6
Leontios Mavropalias <0xLeo@users.noreply.github.com>
2020-09-14 23:40:54 +0200

    add cleanup method

--- latex-templates/taking-notes/compile.py
+++ latex-templates/taking-notes/compile.py
@@ -1,11 +1,32 @@
-#!/usr/bin/python
+!/usr/bin/python
+import os
 import subprocess, sys
+import glob
 
-commands = [
-    ['pdflatex', '-shell-escape' , sys.argv[1] + '.tex'],
-    ['biber', sys.argv[1] + '.bcf'], 
-    ['pdflatex', '-shell-escape' , sys.argv[1] + '.tex'],
-]
 
-for c in commands:
-    subprocess.call(c)
+def cleanup():
+    exts = ['*.log', '*.bcf', '*.run.xml', '*.toc', '*.aux', '*.out', '*.bbl',
+            '*.blg', '_minted-main']
+    to_remove = [glob.glob(e)[0] for e in exts if len(glob.glob(e)) != 0]
+    cmd = ['rm', '-rf', *to_remove]
+    subprocess.call(cmd)
+
+
+def compile(fname = 'main.tex'):
+    fname_no_ext = fname.split('.')[0]
+    commands = [
+        ['pdflatex', '-shell-escape' , fname_no_ext + '.tex'],
+        ['biber', fname_no_ext + '.bcf'], 
+        ['pdflatex', '-shell-escape' , fname_no_ext + '.tex'],
+    ]
+    for c in commands:
+        subprocess.call(c)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        compile()
+    else:
+        assert os.path.isfile(sys.argv[1]), "Please provide a .tex file to compile"
+        compile(sys.argv[1])
+    cleanup()
