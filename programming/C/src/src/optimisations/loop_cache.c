2db86a9a92335efad7b3d61886b8923afc5a9079
0xleo <0xleo.git@gmail.com>
2021-04-24 23:47:37 +0200

    Add some C optimisation notes

---
+++ programming/C/src/src/optimisations/loop_cache.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h> // sleep()
+
+
+int foo() {
+	sleep(1);
+	return 1337;
+}
+
+
+int main(int argc, char *argv[])
+{
+	// Don't do this
+	//for (int i = 0; i < foo(); i++)	
+	// Do this instead
+	for (int len = foo(), i = 0; i < len; i++)	
+		puts(".");
+	return 0;
+}
